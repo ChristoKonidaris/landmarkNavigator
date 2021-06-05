4e0700c0c7e6dd6d11bc4c07808e1c3c0098726f
Christo <christokonidaris@gmail.com>
2021-06-05 13:13:47 +0200

    Added Map fragment

---
+++ app/src/main/java/com/example/landmarknavigator/MapFragment.java
@@ -0,0 +1,92 @@
+package com.example.landmarknavigator;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class MapFragment extends Fragment {
+
+
+    public MapFragment() {
+        // Required empty public constructor
+    }
+
+    public static MapFragment newInstance(String param1, String param2) {
+        MapFragment fragment = new MapFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_map, container, false);
+
+        //Initialize view
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        //Initialize map fragment
+        SupportMapFragment supportMapFragment = (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.google_map);
+
+        //Async map
+        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                MarkerOptions markerOptions = new MarkerOptions();
+                LatLng latLng = new LatLng(LocationService.lat, LocationService.lon);
+                markerOptions.position(latLng);
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                        latLng, 10
+                ));
+                markerOptions.title(LocationService.lat + " : " + LocationService.lon);
+                googleMap.addMarker(markerOptions);
+                //when map is loaded
+                /*googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+
+                    @Override
+                    public void onMapClick(LatLng latLng) {
+                        //When click on map
+                        //Initialize marker options
+                        MarkerOptions markerOptions = new MarkerOptions();
+                        //set position of marker
+                        markerOptions.position(latLng);
+                        //set title of marker
+                        markerOptions.title(LocationService.lat + " : " + LocationService.lon);
+                        //remove all markers
+                        googleMap.clear();
+                        //Animating zoom
+                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                                latLng, 10
+                        ));
+                        //add marker on map
+                        googleMap.addMarker(markerOptions);
+                    }
+
+                });*/
+            }
+        });
+
+        //return view
+        return view;
+
+    }
+}
--- app/src/main/java/com/example/landmarknavigator/SettingsFragment.java
+++ app/src/main/java/com/example/landmarknavigator/SettingsFragment.java
@@ -2,11 +2,15 @@ package com.example.landmarknavigator;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import org.jetbrains.annotations.NotNull;
 
 public class SettingsFragment extends Fragment {
 
@@ -27,7 +31,16 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
     }
 }
