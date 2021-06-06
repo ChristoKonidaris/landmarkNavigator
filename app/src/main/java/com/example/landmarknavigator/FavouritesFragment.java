b90152d6d20e3be8ac626e1fc99fd83c1b199392
Christo <christokonidaris@gmail.com>
2021-06-06 13:09:05 +0200

    Added Favourite  Fragment

---
+++ app/src/main/java/com/example/landmarknavigator/FavouritesFragment.java
@@ -0,0 +1,50 @@
+package com.example.landmarknavigator;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FavouritesFragment extends Fragment {
+
+    //view variables
+    RecyclerView recyclerView;
+
+
+    public FavouritesFragment() {
+        // Required empty public constructor
+    }
+
+    public static FavouritesFragment newInstance(String param1, String param2) {
+        FavouritesFragment fragment = new FavouritesFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favourites, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        recyclerView = view.findViewById(R.id.recyclerView);
+    }
+
+
+}
